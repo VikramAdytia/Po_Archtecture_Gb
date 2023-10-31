69dd1632071bfd274e6e9fa77d028fb0809ec3ef
VikramAdytia <Rundarreg@yandex.ru>
2023-10-31 16:46:07 +0300

    copied base

---
+++ ArchLesson5/src/main/java/ru/geekbrains/lesson5/UILayer.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson5;
+
+public interface UILayer {
+
+    void openProject(String fileName);
+    void showProjectSettings();
+    void saveProject();
+    void printAllModels();
+    void printAllTextures();
+    void renderAll();
+    void renderModel(int i);
+
+}
